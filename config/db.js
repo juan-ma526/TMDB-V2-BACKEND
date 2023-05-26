e1854c56369bcb5bac3cf5730d57daf46d202c98
juan-ma526 <jmperez675@gmail.com>
2023-05-26 12:24:17 -0300

    fix bug db

--- config/db.js
+++ config/db.js
@@ -5,10 +5,18 @@ const db = new Sequelize(
   "tmdb_v2_user",
   "ljEqxLJhMzYiYHWhnu5xvGtkRaGXVYsl",
   {
-    host: "dpg-choc5cqk728h7ntq7vvg-a",
+    host: "dpg-choc5cqk728h7ntq7vvg-a.oregon-postgres.render.com",
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true, // Requerir SSL
+        rejectUnauthorized: false, // No verificar el certificado SSL
+      },
+    },
     logging: false,
   }
 );
 
 module.exports = db;
+
+/* postgres://tmdb_v2_user:ljEqxLJhMzYiYHWhnu5xvGtkRaGXVYsl@dpg-choc5cqk728h7ntq7vvg-a.oregon-postgres.render.com/tmdb_v2 */
