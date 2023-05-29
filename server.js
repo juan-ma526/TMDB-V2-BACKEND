d8b8b034465324dbfda8fbb41c863dcfd18ffc4b
juan-ma526 <jmperez675@gmail.com>
2023-05-29 18:25:08 -0300

    fix cors

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  next();
+});
 
 app.use((err, req, res, next) => {
   res.status(500).send(err.message);
